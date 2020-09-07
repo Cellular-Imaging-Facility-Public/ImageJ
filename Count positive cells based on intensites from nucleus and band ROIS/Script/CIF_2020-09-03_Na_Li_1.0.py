80263dff887141110a548237bb6c0dac496378ae
Yannick Krempp <Yannick.Krempp@gmail.com>
2020-09-07 15:08:32 +0200

    Update CIF_2020-09-03_Na_Li_1.0.py
    Changelog :
    - Works with RGB and Composite images
    - Works with stacks
    - Fixed "unable to make band" by removing ROIs close to the border
    
    TODO
    - Result window :  update green ROIS (only visible on first slice)
    - Set border parameter as a function of the band thickness
    - Option to check only MR+ ROIs for NCC signal (speed increase)
    - Save results as csv or log file

--- Count positive cells based on intensites from nucleus and band ROIS/Script/CIF_2020-09-03_Na_Li_1.0.py
+++ Count positive cells based on intensites from nucleus and band ROIS/Script/CIF_2020-09-03_Na_Li_1.0.py
@@ -1,16 +1,20 @@
 #@ UpdateService updateService
 #@ String (visibility=MESSAGE, value="<html><i>Script written by Yannick Krempp, CIF, 2020</i></html>", required=false) msg0
 #@ String (visibility=MESSAGE, value="<html><br/><br/><p><b>Purpose of this script:</b></p><li>1) Identifies nuclei with Stardist2D</li><li>2) Identifies NCC-positive cells defined by <code>NCC_threshold</code></li><li>3) From NCC-positive cells, counts MR-positive cells defined by <code>MR_threshold</code></li></ul></html>", required=false) msg1
-#@ Integer (label="Band thickness", value=10, style="slider", min=0, max=15, stepSize=1, description="Thickness of the band around the nuclei in pixels") band_thickness
+#@ Integer (label="Band thickness", value=5, style="slider", min=0, max=15, stepSize=1, description="Thickness of the band around the nuclei in pixels") band_thickness
 #@ Integer (label="NCC threshold", value=2000, description="Intensity threshold for NCC") ncc_threshold
 #@ Integer (label="MR threshold", value=1000, description="Intensity threshold for MR") mr_threshold
-#@ Integer (label="Nucleus area", value=200, description="<html>Minimum area for the nucleus in pixels<sup>2</sup></html>") min_nucleus_area
+#@ Integer (label="Min Nucleus area", value=20, description="<html>Minimum area for the nucleus in pixels<sup>2</sup></html>") min_nucleus_area
+#@ Integer (label="Max Nucleus area", value=100, description="<html>Minimum area for the nucleus in pixels<sup>2</sup></html>") max_nucleus_area
+
 
 
 from ij import IJ
 from ij.plugin.frame import RoiManager
 from ij.plugin.filter import Analyzer
 from ij import WindowManager as wm
+from ij.macro import Interpreter as IJ1 
+
 
 RM = RoiManager()        # we create an instance of the RoiManager class
 rm = RM.getRoiManager()  # "activate" the RoiManager otherwise it can behave strangely 
@@ -30,7 +34,8 @@ def initialize():
 	#Step 0: cleanup and sanity check
 	IJ.log("Parameters used:")
 	IJ.log("thickness: "+str(band_thickness))
-	IJ.log("area: "+str(min_nucleus_area))
+	IJ.log("min area: "+str(min_nucleus_area))
+	IJ.log("max area: "+str(max_nucleus_area))
 	IJ.log("NCC threshold: "+str(ncc_threshold))
 	IJ.log("MR threshold: "+str(mr_threshold))
 	IJ.log("----")
@@ -41,24 +46,37 @@ def initialize():
 	IJ.run("Set Scale...", "distance=0 known=0 pixel=1 unit=pixel")
 	IJ.log("Initialize done.")
 
-	#Step1: Getting image information
+def prepareImageComposite():
 	ImageTitle = wm.getCurrentImage().getTitle()
+	IJ.run("Re-order Hyperstack ...", "channels=[Channels (c)] slices=[Frames (t)] frames=[Slices (z)]");
 	IJ.run("Split Channels");
-	#Channel_1 = ImageTitle+" (red)"
-	#Channel_2 = ImageTitle+" (green)"
-	#Channel_3 = ImageTitle+" (blue)"
 	Channel_1 = "C1-"+ImageTitle
 	Channel_2 = "C2-"+ImageTitle
 	Channel_3 = "C3-"+ImageTitle
-	
+	wm.getImage(Channel_1).setTitle("MR")
+	wm.getImage(Channel_2).setTitle("NCC")
+	wm.getImage(Channel_3).setTitle("Nuclei")
+	IJ.log("Composite image prepared")
+
+def prepareImageRGB():
+	ImageTitle = wm.getCurrentImage().getTitle()
+	IJ.run("Re-order Hyperstack ...", "channels=[Channels (c)] slices=[Frames (t)] frames=[Slices (z)]");
+	IJ.run("Split Channels");
+	Channel_1 = ImageTitle+" (red)"
+	Channel_2 = ImageTitle+" (green)"
+	Channel_3 = ImageTitle+" (blue)"
 	wm.getImage(Channel_3).setTitle("MR")
 	wm.getImage(Channel_2).setTitle("NCC")
 	wm.getImage(Channel_1).setTitle("Nuclei")
+	IJ.log("RGB image prepared")
+
+def prepareImageSplit():
+	exit
 
 def detectNuclei(channel):
 	IJ.log("Detecting nuclei with Stardist2D...")
 	wm.getImage(channel)
-	IJ.run("Command From Macro", "command=[de.csbdresden.stardist.StarDist2D], args=['input':"+channel+", 'modelChoice':'Versatile (fluorescent nuclei)', 'normalizeInput':'true', 'percentileBottom':'1.0', 'percentileTop':'99.8', 'probThresh':'0.479071', 'nmsThresh':'0.3', 'outputType':'Both', 'nTiles':'1', 'excludeBoundary':'2', 'roiPosition':'Automatic', 'verbose':'false', 'showCsbdeepProgress':'false', 'showProbAndDist':'false'], process=[false]");
+	IJ.run("Command From Macro", "command=[de.csbdresden.stardist.StarDist2D], args=['input':"+channel+", 'modelChoice':'Versatile (fluorescent nuclei)', 'normalizeInput':'true', 'percentileBottom':'1.0', 'percentileTop':'99.8', 'probThresh':'0.479071', 'nmsThresh':'0.3', 'outputType':'Both', 'nTiles':'1', 'excludeBoundary':'15', 'roiPosition':'Automatic', 'verbose':'false', 'showCsbdeepProgress':'false', 'showProbAndDist':'false'], process=[false]");
 	wm.getImage(channel)
 	rm.runCommand("Show All without labels")
 	IJ.log("Nuclei detected.")
@@ -66,9 +84,10 @@ def detectNuclei(channel):
 	IJ.run("Tile", "")
 
 
-def filter_small_nuclei():
+def filter_nuclei():
 
 	area_idx = []
+	area = 0
 	stardist_rois = rm.getCount()
 	IJ.run("Set Measurements...", "area mean redirect=NCC decimal=3")
 	IJ.selectWindow("Nuclei")
@@ -77,9 +96,12 @@ def filter_small_nuclei():
 	
 	for nuclei in range(0,stardist_rois):
 		roi = rm.select(nuclei)
-		if IJ.getValue(wm.getImage("NCC"), "Area") > min_nucleus_area:
+		area = IJ.getValue(wm.getImage("NCC"), "Area")
+		if ((area > min_nucleus_area) and (area < max_nucleus_area)) :
 			area_idx.append(nuclei)
-	IJ.log("Number of cells: "+str(len(area_idx)))
+		else:
+			rm.rename(nuclei, str(nuclei)+"-Excluded_(area)")
+	IJ.log("Number of cells fitting area criteria: "+str(len(area_idx)))
 	return area_idx
 
 def detect_MR_pos(nuclei):
@@ -90,15 +112,18 @@ def detect_MR_pos(nuclei):
 	IJ.run("Set Measurements...", "area mean redirect=MR decimal=3")
 	pos_idx = []
 	neg_idx = []
+	idx=0
 	
 	for idx in nuclei:
 		rm.select(idx)
 		MR_mean = IJ.getValue(wm.getImage("MR"), "Mean")
 		if MR_mean > mr_threshold:
 			pos_idx.append(idx)
+			rm.rename(idx, str(idx)+"-MR-POS")
 			IJ.run(IJ.getImage(), "Add Selection...", "")
 		else:
 			neg_idx.append(idx)
+			rm.rename(idx, str(idx)+"-MR-NEG")
 			
 	IJ.run("Show Overlay", "")
 	IJ.log("Number of MR+ cells: "+str(len(pos_idx)))
@@ -108,25 +133,30 @@ def detect_MR_pos(nuclei):
 def detect_NCC_pos(nuclei, thickness):
 			
 	IJ.selectWindow("NCC")
+	imp = IJ.getImage()
 	IJ.run("Remove Overlay", "")
 	IJ.run("Overlay Options...", "stroke=green width=0 fill=green set")
 	IJ.run("Set Measurements...", "area mean redirect=NCC decimal=3");
 	pos_idx = []
 	neg_idx = []
-	
+	idx=0
+	#IJ.log("nuclei :"+str(len(nuclei)))
+	IJ1.batchMode = True 
 	for idx in nuclei:
-		
+		#IJ.log("idx: "+str(idx))
 		ncc_roi = rm.select(idx)
-		imp = IJ.getImage()
 		IJ.run("Make Band...", "band="+str(thickness))
 		#rm.runCommand("Update")
 		if IJ.getValue(wm.getImage("NCC"), "Mean") > ncc_threshold:
 			pos_idx.append(idx)
+			rm.rename(idx, str(idx)+"-NCC-POS")
 			IJ.run(imp, "Add Selection...", "")
 			IJ.run(imp, "Select None", "")
 		else:
 			neg_idx.append(idx)
-			
+			rm.rename(idx, str(idx)+"-NCC-NEG")
+	IJ1.batchMode = False 
+		
 	IJ.run("Show Overlay", "")
 	IJ.log("Number of NCC+ cells: "+str(len(pos_idx)))
 	IJ.log("Number of NCC- cells: "+str(len(neg_idx)))
@@ -170,14 +200,16 @@ else:
 	
 	original_image = wm.getCurrentImage().duplicate()
 	original_image.setTitle("Original")
-	
 	initialize()
+	
+	if (original_image.isComposite()):
+		prepareImageComposite()
+	else:
+		prepareImageRGB()
+	
 	detectNuclei("Nuclei")
-	cells_idx = filter_small_nuclei()
+	cells_idx = filter_nuclei()
 	mr_pos_idx = detect_MR_pos(cells_idx)
 	ncc_pos_idx = detect_NCC_pos(cells_idx, band_thickness)
 	displayResults(original_image, mr_pos_idx, ncc_pos_idx)
 	cleanup()
-
-
-
